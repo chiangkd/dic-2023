4c133f2b84a0f132141b4a347c91a9ede6d62919
chiangkd <chiangkuandi@gmail.com>
2023-04-23 21:59:52 +0800

    Pass verification

--- HW3/file/AEC.v
+++ HW3/file/AEC.v
@@ -10,14 +10,10 @@ input [7:0] ascii_in;
 output reg valid;
 output reg [6:0] result;
 
-
-
 reg [6:0] data [0:15];
 reg [3:0] data_index, data_num;
-
 reg [6:0] string [0:15];  // output string
 reg [6:0] stack [0:15];   // stack
-
 reg [3:0] string_index, stack_index;
 
 localparam DATA_IN = 3'd0;
@@ -33,22 +29,6 @@ initial begin
     data_num = 4'd0;
     string_index = 4'd0;
     stack_index = 4'd0;
-end
-
-always @(*) begin
-    case(CurrentState)
-        DATA_IN:begin
-        end
-        POPMULT:begin
-            
-        end
-        CALCULATION:begin
-            
-        end
-        OUTPUT:begin
-            
-        end
-    endcase
 end
 
 /* State register (Sequential) */
@@ -77,24 +57,41 @@ always @(posedge clk) begin
                      * a(10) ~ f(15)    => 0110_0001 ~ 0110_0110        (translate to binary code represent decimal, 0000_1010 ~ 0000_1111)
                      */
                     data[data_index] <= (ascii_in[6:4] ^ 3'b010) ? ((ascii_in[4] & 1'b1) ?  (ascii_in[3:0]): (ascii_in[3:0] + 4'b1001)) : ascii_in[6:0];
-                    // data[data_index] <= (ascii_in >= 97 && ascii_in <= 102) ? ascii_in - 40: ascii_in; // pre-offset the offset of ASCII code
                     data_index <= data_index + 1; 
                 end
             end
             POPSINGLE: begin
-                if((data[data_index] >= 40) && (data[data_index] <= 45)) begin  // detect operator
-                    if((stack[stack_index - 1] &1'b1) <= (data[data_index] & 1'b1)) begin    // pop out
+                if(string_index == data_num) begin
+                    data_num <= string_index - 1;   // reuse number counter, the counter will be string index - 1
+                    string_index <= 0; // reset string index and next do the calculation
+                end
+                if((data[data_index] >= 42) && (data[data_index] <= 45)) begin  // detect operator
+                    if(((stack[stack_index - 1] &1'b1) <= (data[data_index] & 1'b1)) && stack[stack_index - 1] != 40) begin    // pop out
                         string[string_index] <= stack[stack_index - 1]; // push the popped element to string
                         string_index <= string_index + 1;
-                        // stack_index = stack_index - 1;  // pop stack
+                        stack_index <= stack_index - 1;  // pop stack
                         stack[stack_index - 1] <= data[data_index];
                         /* TODO: FIX double pop problem */
+                        if((stack[stack_index - 1] &1'b1) > (data[data_index] & 1'b1) || (stack_index == 0)) stack_index <= stack_index + 1;
                     end
                     else if(string_index + stack_index < data_num) begin  // detect number, push to stack
                         stack[stack_index] <= data[data_index]; // push data to stack
                         stack_index <= stack_index + 1; // push stack
+                        data_index <= data_index + 1;
                     end
+                end
+                else if(data[data_index] == 40) begin   // if detect "(", force push
+                    stack[stack_index] <= data[data_index];
+                    stack_index <= stack_index + 1;
                     data_index <= data_index + 1;
+                    data_num <= data_num - 1;
+                end
+                else if(data[data_index] == 41) begin   // if detect ")", force pop stack
+                    string[string_index] <= stack[stack_index - 1];
+                    string_index <= string_index + 1;
+                    stack_index <= stack_index - 1;
+                    data_index <= data_index + 1;
+                    data_num <= data_num - 1;
                 end
                 else if(string_index + stack_index < data_num) begin
                     string[string_index] <= data[data_index];
@@ -103,13 +100,9 @@ always @(posedge clk) begin
                 end
             end
             POPMULT: begin
-                if(data[data_index] == 41)begin // detect ")"
-                    if(data[data_index] != 40) begin
-                        string[string_index] <= stack[stack_index];
-                        string_index <= string_index + 1;
-                        stack_index <= stack_index - 1;
-                    end
-                    else stack_index <= stack_index - 1;    // discard "("
+                if(stack[stack_index - 1] == 40) begin  // discard "("
+                    stack_index <= stack_index - 1;
+                    // data_index <= data_index - 2;
                 end
                 else if(stack_index) begin  // pop until stack is empty
                     string[string_index] <= stack[stack_index - 1];
@@ -159,12 +152,15 @@ always @(*) begin
             else NextState = DATA_IN;
         end
         POPSINGLE: begin
-            if(string_index + stack_index == data_num) NextState = POPMULT;
+            if(string_index == data_num && !stack_index) NextState = CALCULATION;
+            else if(string_index + stack_index == data_num) NextState = POPMULT;
             else if(data[data_index] == 41) NextState = POPMULT;    // detect ")"
             else NextState = POPSINGLE;
         end
         POPMULT:begin // check to pop stack 
-            if(!stack_index) NextState = CALCULATION;
+            if(stack[stack_index - 1] == 40) NextState = POPSINGLE;
+            else if(!stack_index) NextState = CALCULATION;
+            // else if(stack[stack_index - 1] == 40) NextState = POPSINGLE;
             else NextState = POPMULT;
         end
         CALCULATION:begin
@@ -177,12 +173,5 @@ always @(*) begin
         end
     endcase
 end
-
-
-/* Output logic (Combinational) */
-
-
-//-----Your design-----//
-
 
 endmodule
